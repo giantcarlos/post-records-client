b69d7bae69a6963ff98da3e9a87e8dc2823725a2
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-05-16 17:50:53 -0500

    set up app and index

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Shop from './components/Shop';
+import Collection from './components/Collection';
+import Record from './components/Record';
+import RecordCollected from './components/RecordCollected';
+import Navigation from './components/Navigation';
+import CollectionForm from './components/CollectionForm';
 
-function App() {
+
+const App = () => {
+  const [login, setLogin] = useState(false);
+  const [records, setRecords] = useState([]);
+  const [collection, setCollection] = useState([]);
+
+    useEffect(() => {
+        fetch("https://post-records-server.onrender.com/records")
+        .then(res => res.json())
+        .then(data => setRecords(data))
+    }, [])
+
+   const handleLogin = () => {
+    login === false ? setLogin(true) : setLogin(false);
+   }
+
+   const addToCollection = (newRecord) => {
+    setCollection(collection => [...collection, newRecord])
+   }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <Navigation login={login} handleLogin={handleLogin}/>
+       <Switch>
+        <Route exact path="/"><Home /></Route>
+        <Route exact path="/about"><About /></Route>
+        <Route exact path="/shop"><Shop records={records}/></Route>
+        <Route exact path="/collection"><Collection collection={collection} setCollection={setCollection} login={login}/></Route>
+        <Route exact path="/shop/:id"><Record addtoCollection={addToCollection} login={login}/></Route>
+        <Route exact path="/collection/:id"><RecordCollected /></Route>
+        <Route exact path="/collectionform"><CollectionForm collection={collection} setCollection={setCollection} addtoCollection={addToCollection}/></Route>
+       </Switch>
     </div>
-  );
+  )
 }
 
 export default App;
